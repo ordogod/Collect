9c869283f87adae30ac770d9cc9fb11b6289cfd7
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-09-27 14:11:45 +0300

    Add 'NoteTypesAdapter'

---
+++ app/src/main/java/com/ordolabs/collect/ui/adapter/NoteTypesAdapter.kt
@@ -0,0 +1,54 @@
+package com.ordolabs.collect.ui.adapter
+
+import android.view.View
+import androidx.annotation.StringRes
+import androidx.appcompat.widget.AppCompatTextView
+import com.ordolabs.collect.R
+import com.ordolabs.collect.ui.adapter.base.BaseAdapter
+import com.ordolabs.collect.ui.adapter.base.BaseViewHolder
+import com.ordolabs.collect.ui.adapter.base.OnRecyclerItemClicksListener
+import com.ordolabs.collect.util.viewId
+import com.ordolabs.collect.viewmodel.ItemBuilderViewModel.ItemType
+
+class NoteTypesAdapter(
+    clicksListener: OnRecyclerItemClicksListener<ItemType>
+) : BaseAdapter<ItemType, NoteTypesAdapter.TypeViewHolder>(clicksListener) {
+
+    val types: List<ItemType> = ItemType.getExpandedList()
+
+    override fun setItems(items: List<ItemType>) {
+        // items are defined already
+    }
+
+    override fun onBindViewHolder(holder: TypeViewHolder, position: Int) {
+        val item = types[position]
+        holder.onBind(item)
+    }
+
+    override fun getItemViewLayout(viewType: Int): Int {
+        return R.layout.item_create_item_type_single
+    }
+
+    override fun createViewHolder(itemView: View): TypeViewHolder {
+        return TypeViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return types.size
+    }
+
+    class TypeViewHolder(root: View) : BaseViewHolder<ItemType>(root) {
+
+        private val name by itemView.viewId<AppCompatTextView>(R.id.item_type_single_name)
+
+        override fun setViewsOnBind(item: ItemType) {
+            setTypeName(item.label)
+        }
+
+        private fun setTypeName(@StringRes itemLabel: Int) {
+            val typeName = itemView.context.getString(itemLabel)
+            name.text = typeName
+        }
+
+    }
+}
