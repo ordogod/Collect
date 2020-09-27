e7ae814bb63082f5eecf77bee0a4cfe8f63665a6
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-09-27 15:41:08 +0300

    Add lazy for RecyclerView.ViewHolder

--- app/src/main/java/com/ordolabs/collect/util/LazyUtil.kt
+++ app/src/main/java/com/ordolabs/collect/util/LazyUtil.kt
@@ -4,6 +4,7 @@ import android.view.View
 import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 
 // do not remove explicit type argument — causes build crash
 
@@ -18,3 +19,6 @@ fun <T : View> Fragment.viewId(@IdRes id: Int): Lazy<T> =
 
 fun <T : View> AppCompatActivity.viewId(@IdRes id: Int): Lazy<T> =
     lazyUnsafe { findViewById<T>(id) }
+
+fun <T : View> RecyclerView.ViewHolder.viewId(@IdRes id: Int): Lazy<T> =
+    lazyUnsafe { itemView.findViewById<T>(id) }
