abd7996706b869cbf26a0a81c70198a8bbfcbadf
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-09-27 16:51:36 +0300

    Implement item expanding and collapsing feature

--- app/src/main/java/com/ordolabs/collect/ui/adapter/ItemTypesAdapter.kt
+++ app/src/main/java/com/ordolabs/collect/ui/adapter/ItemTypesAdapter.kt
@@ -2,7 +2,6 @@ package com.ordolabs.collect.ui.adapter
 
 import android.view.View
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.annotation.StringRes
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.view.isInvisible
@@ -17,8 +16,9 @@ class ItemTypesAdapter(
     clicksListener: OnRecyclerItemClicksListener
 ) : BaseAdapter<ItemType, ItemTypesAdapter.TypeViewHolder>(clicksListener) {
 
-    private val types: List<ItemType> = ItemType.expandedList
-    private val typesExpanded: List<Boolean> = MutableList(types.size) { false }
+    private val types: MutableList<ItemType> = ItemType.getCollapsedList()
+    private val typesCollapsed: List<ItemType> = ItemType.getCollapsedList()
+    private val isTypeExpanded = MutableList(typesCollapsed.size) { false }
 
     override fun setItems(items: List<ItemType>) {
         // items are defined already
@@ -27,6 +27,41 @@ class ItemTypesAdapter(
     override fun onBindViewHolder(holder: TypeViewHolder, position: Int) {
         val item = types[position]
         holder.onBind(item)
+    }
+
+    override fun performClick(holder: TypeViewHolder) {
+        if (!holder.isExpandable) return
+//        val index = types.indexOf(holder.boundItem)
+        val index = holder.bindingAdapterPosition
+        val isExpanded = isTypeExpanded[index]
+
+        holder.toggleExpand(isExpanded)
+
+        if (isExpanded) {
+            collapseItem(index)
+        } else {
+            expandItem(index)
+        }
+    }
+
+    private fun expandItem(itemIndex: Int) {
+        val targetItem = types[itemIndex]
+        val subtypes = targetItem.children
+        val insertIndex = itemIndex + 1
+
+        types.addAll(insertIndex, subtypes)
+        isTypeExpanded[itemIndex] = true
+        notifyItemRangeInserted(insertIndex, subtypes.size)
+    }
+
+    private fun collapseItem(itemIndex: Int) {
+        val targetItem = types[itemIndex]
+        val subtypes = targetItem.children
+        val removeIndex = itemIndex + 1
+
+        types.removeAll(subtypes)
+        isTypeExpanded[itemIndex] = false
+        notifyItemRangeRemoved(removeIndex, subtypes.size)
     }
 
     override fun getItemViewLayout(viewType: Int): Int {
@@ -38,13 +73,16 @@ class ItemTypesAdapter(
     }
 
     override fun getItemCount(): Int {
-        return ItemType.collapsedCount
+        return types.size
     }
 
     class TypeViewHolder(root: View) : BaseViewHolder<ItemType>(root) {
 
         private val name by viewId<AppCompatTextView>(R.id.item_create_type_name)
         private val dropdown by viewId<ImageView>(R.id.item_create_type_dropdown)
+
+        val isExpandable: Boolean
+            get() = boundItem.children.isNotEmpty()
 
         override fun setViewsOnBind(item: ItemType) {
             setTypeName(item.label)
@@ -60,10 +98,9 @@ class ItemTypesAdapter(
             dropdown.isInvisible = !hasChildren
         }
 
-        override fun onClick(v: View?) {
-            if (boundItem.children.isEmpty()) return
-
-            Toast.makeText(v?.context, "open", Toast.LENGTH_SHORT).show()
+        fun toggleExpand(isExpanded: Boolean) {
+            dropdown.rotation += 180f
+            if (dropdown.rotation == 360f) dropdown.rotation = 0f
         }
     }
 }
