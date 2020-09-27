6891fc7c5f4b7338765ae24d333cbfe99fc70f5b
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-09-27 13:49:26 +0300

    Add KDoc

--- app/src/main/java/com/ordolabs/collect/ui/activity/StartableActivity.kt
+++ app/src/main/java/com/ordolabs/collect/ui/activity/StartableActivity.kt
@@ -3,7 +3,13 @@ package com.ordolabs.collect.ui.activity
 import android.content.Context
 import android.content.Intent
 
+/**
+ * Must be inherited by `Activity`'s `companion object`.
+ */
 interface StartableActivity {
 
+    /**
+     * Creates new [Intent] and configures it (sets extras etc.).
+     */
     fun getStartIntent(caller: Context): Intent
 }
