684cb52b1a1dd5530ce142cd267be59f1e1069c1
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-09-27 13:44:24 +0300

    Add KDoc

--- app/src/main/java/com/ordolabs/collect/ui/adapter/base/BaseAdapter.kt
+++ app/src/main/java/com/ordolabs/collect/ui/adapter/base/BaseAdapter.kt
@@ -7,10 +7,12 @@ import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
+/**
+ * An abstract implementation of [RecyclerView.Adapter].
+ */
 abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T>>(
     var clicksListener: OnRecyclerItemClicksListener<T>
-) :
-    RecyclerView.Adapter<VH>() {
+) : RecyclerView.Adapter<VH>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         val root = createItemView(parent, viewType)
@@ -23,27 +25,46 @@ abstract class BaseAdapter<T : Any, VH : BaseViewHolder<T>>(
         super.onViewAttachedToWindow(holder)
     }
 
+    /**
+     * Specifies way of creating instances of [VH].
+     */
     protected abstract fun createViewHolder(itemView: View): VH
 
+    /**
+     * Specifies layout ID res, according to [viewType].
+     */
     @LayoutRes
     protected abstract fun getItemViewLayout(viewType: Int): Int
 
+    /**
+     * Configurates specified [holder] and its view.
+     */
+    @CallSuper
     protected open fun setViewHolder(holder: VH) {
         holder.itemView.setOnClickListener { onViewHolderClick(holder) }
         holder.itemView.setOnLongClickListener { onViewHolderLongClick(holder) }
     }
 
+    /**
+     * Would be called on [VH]'s view click.
+     */
     @CallSuper
     protected open fun onViewHolderClick(holder: VH) {
         holder.performClick()
     }
 
+    /**
+     * Would be called on [VH]'s view long click.
+     */
     protected open fun onViewHolderLongClick(holder: VH): Boolean {
         holder.performLongClick()
         return false
     }
 
-    private fun createItemView(parent: ViewGroup, viewType: Int): View {
+    /**
+     * Inflates [getItemViewLayout] view.
+     */
+    protected open fun createItemView(parent: ViewGroup, viewType: Int): View {
         val layout = getItemViewLayout(viewType)
         return LayoutInflater.from(parent.context).inflate(layout, parent, false)
     }
