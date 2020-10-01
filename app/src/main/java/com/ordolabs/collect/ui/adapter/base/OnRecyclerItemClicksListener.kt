2254ee3790e3e44edbfafbb4ff2020a9091c066e
mikhail.molosay <mikhail.molosay@effective-soft.com>
2020-10-01 23:07:19 +0300

    Add default empty implementation for RecyclerView s as just a list

--- app/src/main/java/com/ordolabs/collect/ui/adapter/base/OnRecyclerItemClicksListener.kt
+++ app/src/main/java/com/ordolabs/collect/ui/adapter/base/OnRecyclerItemClicksListener.kt
@@ -2,7 +2,9 @@ package com.ordolabs.collect.ui.adapter.base
 
 interface OnRecyclerItemClicksListener {
 
-    fun onRecyclerItemClick(position: Int)
+    fun onRecyclerItemClick(position: Int) {
+        // default empty implementation
+    }
 
     fun onRecyclerItemLongClick(position: Int) {
         // default empty implementation
